03f8559811c754fc48e8c0a7cb4c15a2d4968c60
EduardoGuaglioniLupianez <eduardo.lupianez@sptech.school>
2026-05-11 21:06:24 -0300

    algumas alterações no css do calendario

---
+++ public/js/dashboard.js
@@ -0,0 +1,40 @@
+let mes = id_mes.value
+let ano = id_ano.value
+console.log(mes, ano)
+
+
+function teladiacalendario(dia, mes, ano) {
+    // fazer uma função que compare o dia, ano e mes quem foi selecionado com algum do banco de dados
+
+
+
+
+
+}
+function mes_ano(){
+    let meses = ["janeiro","fevereiro"]
+    if(ano = "2027"){
+        for (let i = 0; i < array.length; i++) {
+            const element = array[i];
+            
+        }
+
+
+
+    }
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
