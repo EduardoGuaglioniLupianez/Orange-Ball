0889efed1305726dab6642b02a95f5892e9a0d7b
EduardoGuaglioniLupianez <eduardo.lupianez@sptech.school>
2026-05-29 20:12:26 -0300

    arrumando app.js

--- public/js/alerta.js
+++ public/js/alerta.js
@@ -95,7 +95,7 @@ function removerAlerta(idAquario) {
 
 function exibirCards() {
     alerta.innerHTML = '';
-
+aw
     for (var i = 0; i < alertas.length; i++) {
         var mensagem = alertas[i];
         alerta.innerHTML += transformarEmDiv(mensagem);
